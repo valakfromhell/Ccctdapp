5b5c0e0397826911c2e9b79cfa5ef2f84a88a55a
yogendra sharma <119298162+valakfromhell@users.noreply.github.com>
2023-05-30 10:07:40 +0530

    index added

---
+++ src/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>CCCT Chatroom</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3>Login Form</h3>
+     
+  {/if}
+</header>
---
+++ src/Login.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { user } from './user';
+
+  let username;
+  let password;
+
+  function login() {
+    user.auth(username, password, ({ err }) => err && alert(err));
+  }
+
+  function signup() {
+    user.create(username, password, ({ err }) => {
+      if (err) {
+        alert(err);
+      } else {
+        login();
+      }
+    });
+  }
+  export let linkText = 'Click Here For Terms and Conditions';
+  export let url = 'Users/yogendra/ccctdapp/terms.html';
+</script>
+
+<label for="username">Username</label>
+<input name="username" bind:value={username} minlength="3" maxlength="16" />
+
+<label for="password">Password</label>
+<input name="password" bind:value={password} type="password" />
+
+<button class="login" on:click={login}>Login</button>
+<button class="login"  on:click={signup}>Sign Up</button>
+  
+<h4>Please Make Sure To Respect The End User And Not Spam Any Messages Also The Use Of Profanity Is Discouraged</h4>
+
+
+
+<h5>Note: By Signing up you are Agreeing to the Terms and Conditions of CCCT chatroom You Can Read The Terms And Conditions Of CCCT Chatroom By Clicking In the Link Below</h5>
+<a href={url}>{linkText}</a>
